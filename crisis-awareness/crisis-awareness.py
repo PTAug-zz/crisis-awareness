9f6d32035d337f2f763c849d13391901bc51852e
Paul-Tristan Auguin <paultristan.auguin@gmail.com>
2016-11-17 23:42:04 -0500

    Working app on mac.

--- crisis-awareness/crisis-awareness.py
+++ crisis-awareness/crisis-awareness.py
@@ -10,7 +10,7 @@ import gmplot
 mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'MYSQLPT'
+app.config['MYSQL_DATABASE_PASSWORD'] = '------'
 app.config['MYSQL_DATABASE_DB'] = 'flask'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
@@ -24,65 +24,63 @@ class MapParamsForm(Form):
   dtto = DateField('DatePicker', format='%Y-%m-%d', default=date(2016,1,2))
 
 class AnalyticsForm(Form):
-	attributes = SelectField('Data Attributes', choices=[('Agency', 'Agency'), ('Borough', 'Borough'), ('Complaint_Type', 'Complaint Type')])
+    attributes = SelectField('Data Attributes', choices=[('Agency', 'Agency'), ('Borough', 'Borough'), ('Complaint_Type', 'Complaint Type')])
 
 
 def get_homepage_links():
-	return 	[	{"href": url_for('map'), "label":"Draw the Map"},
-						{"href": url_for('analytics'), "label":"Analytics"},
-					]
+    return 	[{"href": url_for('map'), "label":"Draw the Map"},{"href": url_for('analytics'), "label":"Analytics"},]
 def get_data(dtfrom,dtto):
-	query = "select latitude, longitude from incidents where created_date >= '" + dtfrom + "' and created_date <= '" + dtto + "';"
-	cursor.execute(query)
-	return cursor.fetchall()
+    query = "select latitude, longitude from incidents where created_date >= '" + dtfrom + "' and created_date <= '" + dtto + "';"
+    cursor.execute(query)
+    return cursor.fetchall()
 
 def get_df_data():
-	import pandas
-	query = "select unique_key, agency, complaint_type, borough from incidents;"
-	cursor.execute(query)
-	data = cursor.fetchall()
-	df = pandas.DataFrame(data=list(data),columns=['Unique_key','Agency','Complaint Type','Borough'])
-	return df
+    import pandas
+    query = "select unique_key, agency, complaint_type, borough from incidents;"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    df = pandas.DataFrame(data=list(data),columns=['Unique_key','Agency','Complaint Type','Borough'])
+    return df
 
 @app.route("/")
 def home():
-	session["data_loaded"] = True
-	return render_template('home.html', links=get_homepage_links())
+    session["data_loaded"] = True
+    return render_template('home.html', links=get_homepage_links())
 
 @app.route("/map", methods=['GET','POST'])
 def map():
-	form = MapParamsForm()
-	if form.validate_on_submit():
-		dtfrom =  form.dtfrom.data.strftime('%Y-%m-%d')
-		dtto =  form.dtto.data.strftime('%Y-%m-%d')
-		coordinates = get_data(dtfrom, dtto)
-		latitudes, longitudes = ([],[])
-		if (len(coordinates)>0):
-			for pair in coordinates:
-				latitudes.append(pair[0])
-				longitudes.append(pair[1])
-		gmap = gmplot.GoogleMapPlotter.from_geocode("New York",8)
-		gmap.heatmap(latitudes, longitudes)
-		gmap.draw('templates/mapoutput.html')
-		return render_template('map.html', mapfile = 'mapoutput.html')
+    form = MapParamsForm()
+    if form.validate_on_submit():
+        dtfrom =  form.dtfrom.data.strftime('%Y-%m-%d')
+        dtto =  form.dtto.data.strftime('%Y-%m-%d')
+        coordinates = get_data(dtfrom, dtto)
+        latitudes, longitudes = ([],[])
+        if (len(coordinates)>0):
+            for pair in coordinates:
+                latitudes.append(pair[0])
+                longitudes.append(pair[1])
+        gmap = gmplot.GoogleMapPlotter.from_geocode("New York",8)
+        gmap.heatmap(latitudes, longitudes)
+        gmap.draw('templates/mapoutput.html')
+        return render_template('map.html', mapfile = 'mapoutput.html')
 
-	return render_template('mapparams.html', form=form)
+    return render_template('mapparams.html', form=form)
 
 @app.route('/analytics/',methods=['GET','POST'])
 def analytics():
-	form = AnalyticsForm()
-	if form.validate_on_submit():
-		import pandas
-		df = get_df_data()
-		column = request.form.get('attributes')
-		group = df.groupby(column)
-		ax = group.size().plot(kind='bar')
-		fig = ax.get_figure()
-		fig.savefig('static/group_by_fig.png')
-		return render_template('analyticsoutput.html')
+    form = AnalyticsForm()
+    if form.validate_on_submit():
+        import pandas
+        df = get_df_data()
+        column = request.form.get('attributes')
+        group = df.groupby(column)
+        ax = group.size().plot(kind='bar')
+        fig = ax.get_figure()
+        fig.savefig('static/group_by_fig.png')
+        return render_template('analyticsoutput.html')
 
 
-	return render_template('analyticsparams.html', form=form)
+    return render_template('analyticsparams.html', form=form)
 
 if __name__ == '__main__':
    app.run(debug = True)
